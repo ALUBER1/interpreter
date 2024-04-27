4d5e6250bf6ceaf2f7ad5406cd1f9073a944e690
ALUBER1 <140241493+ALUBER1@users.noreply.github.com>
2024-04-27 13:19:12 +0200

    Delete interpretatore directory

--- interpretatore/gestisci/ottieni_dati.py
+++
@@ -1,5 +0,0 @@
-import tkinter
-def ottieni_parola(n=0,testo=0):
-    a=testo.get('1.0',tkinter.END)
-    b=a.split()
-    print( b[n])
--- interpretatore/sposta.bat
+++
@@ -1,7 +0,0 @@
-if not exist "c:\MinGW" (
-	move "MinGW" c:\
-	md MinGW
-	xcopy /s /e /i c:\MinGW MinGW
-	cd interpretatore
-exit
-)
